547c3760a398c897cd0fab925998e84275d124c0
Earzu <huascq@163.com>
2024-06-01 17:35:07 +0800

    史诗级更新版面和目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,118 @@
 module.exports = {
     title: '南武文库',
-    description: '很酷，不说话'
+    description: '很酷，不说话',
+    themeConfig: {
+        nav: [
+            {
+                text: '南武新闻',
+                link: '/news/',
+            }, {
+                text: '南武维基',
+                link: '/wiki/',
+            }, {
+                text: '南武历史',
+                link: '/history/',
+            }, {
+                text: '南武记忆',
+                link: '/memory/',
+            },
+        ],
+        sidebar: {
+            '/history/':
+                [
+                    'ancient',
+                    'modern',
+                    'war',
+                    'revolution',
+                    'party',
+                    'special_zone',
+                    'nambu_group',
+                    'progressivism',
+                    'central_government',
+                ],
+            '/news/':
+                [
+                    '77th_anniversary_of_nambu_defense_war',
+                    'kojun_meeting_with_second_army_political_commissar',
+
+                ],
+            '/wiki/': [
+                {
+                    title: '国家相关',
+                    children: [
+                        'nambu_democratic_republic',
+                        'nambu_united_party',
+                        'nambu_group',
+                        'progressivism',
+                        'central_leadership',
+                        'nambu_congress',
+                        'central_government',
+                        'group_leadership',
+                        'joint_ownership',
+                        'nambu_group_economic_model',
+                        'nambu_special_zone',
+                        'kaijitsu_committee',
+                        'great_nambu_empire',
+                        'nambu_republic',
+                        'nambu_democratic_republican_party',
+                        'takezuka_koku',
+                        'takezuka_group'
+                    ]
+                },
+                {
+                    title: '人物相关',
+                    children: [
+                        'nambu_kojun',
+                        'hinata_akira',
+                        'hayashi_fuyu',
+                        'hisazaki_fumikuni',
+                        'yamamoto_hakuha',
+                        'togishi_norikazu'
+                    ]
+                },
+                {
+                    title: '其它',
+                    children: [
+                        'honbu_metropolis',
+                        'song_of_nambu',
+                        'nambu_army'
+                    ]
+                }
+            ],
+            '/memory/': [
+                {
+                    title: '国家相关',
+                    children: [
+                        'nambu_design'
+                    ]
+                },
+                {
+                    title: '法规相关',
+                    children: [
+                        'constitution',
+                        'party_constitution',
+                        'group_leadership_law',
+                        'special_zone_law'
+                    ]
+                },
+                {
+                    title: '机关公文',
+                    children: [
+                        {
+                            title: '南武联合党中央办公厅',
+                            children: [
+                                'further_promoting_construction_of_planned_economy'
+                            ]
+                        },
+                        {
+                            title: '南武民主共和国中央发展规划委员会',
+                            children: [
+                                'reform_of_group_leadership_system_in_kaijitsu'
+                            ]
+                        }
+                    ]
+                }
+            ],
+        },
+    },
 }
--- docs/readme.md
+++ docs/readme.md
@@ -1,115 +1,30 @@
 # 南武文库
-> 在全世界无产阶级共同的节日五一劳动节来临之际，我们向全体默默努力与奋斗的劳动者祝贺节日快乐！
->
-> 不论这个世界被说成多么美好，但只要剥削、压迫及其根源私有制存在一天，它就不属于劳动人民。让我们继续为阶级解放而奋斗。
->
-> 劳动人民终会推翻万恶的剥削制度，获得自由解放。全世界无产阶级大团结万岁！
 
-欢迎来到南武文库，本文库由志愿者维护，旨在记录南武民主共和国的历史、文化、政治、经济等方面的信息。
+::: danger
+
+### 写在最前
+
+在全世界无产阶级共同的节日五一劳动节来临之际，我们向全体默默努力与奋斗的劳动者祝贺节日快乐！
+
+不论这个世界被说成多么美好，但只要剥削、压迫及其根源私有制存在一天，它就不属于劳动人民。让我们继续为阶级解放而奋斗。
+
+劳动人民终会推翻万恶的剥削制度，获得自由解放。全世界无产阶级大团结万岁！
+:::
+
+欢迎来到南武文库，本文库由志愿者维护，旨在记录方方面面有关南武世界观的内容。
 
 南武文库包括南武新闻、南武维基、南武历史、南武记忆等板块，希望能为您提供全面的南武信息。
 
-## 南武新闻
-[纪念南武卫国战争77周年，南武集团首席执行官林冬发表重要讲话](./news/77th_anniversary_of_nambu_defense_war.md)
+[南武新闻](./history)
 
-[南武香淳老主席亲切接见进步军第二军政委](./news/kojun_meeting_with_second_army_political_commissar.md)
+[南武维基](./wiki)
 
-## 南武维基
-### 国家相关
-[南武民主共和国](./wiki/nambu_democratic_republic.md)
+[南武历史](./history)
 
-[南武联合党](./wiki/nambu_united_party.md)
+[南武记忆](./memory)
 
-[南武集团](./wiki/nambu_group.md)
+::: warning
 
-[南武进步主义](./wiki/progressivism.md)
-
-[中央领导团](./wiki/central_leadership.md)
-
-[全南武进步者大会](./wiki/nambu_congress.md)
-
-[南武中央人民政府](./wiki/central_government.md)
-
-[集团领导制](./wiki/group_leadership.md)
-
-[联合所有制](./wiki/joint_ownership.md)
-
-[南武集团特色计划经济模式](./wiki/nambu_group_economic_model.md)
-
-[南武民主共和国特区](./wiki/nambu_special_zone.md)
-
-[海実学生进步委员会](./wiki/kaijitsu_committee.md)
-
-[大南武帝国](./wiki/great_nambu_empire.md)
-
-[南武共和国](./wiki/nambu_republic.md)
-
-[南武民主共和党](./wiki/nambu_democratic_republican_party.md)
-
-[武塚国](./wiki/takezuka_koku.md)
-
-[武塚集团](./wiki/takezuka_group.md)
-
-### 人物相关
-[南武香淳](./wiki/nambu_kojun.md)
-
-[日向秋平](./wiki/hinata_akira.md)
-
-[林冬](./wiki/hayashi_fuyu.md)
-
-[久崎文国](./wiki/hisazaki_fumikuni.md)
-
-[山本碧羽](./wiki/yamamoto_hakuha.md)
-
-[户岸希节](./wiki/togishi_norikazu.md)
-
-### 其它
-[本部都](./wiki/honbu_metropolis.md)
-
-[南武之歌](./wiki/song_of_nambu.md)
-
-[南武人民进步军](./wiki/nambu_army.md)
-
-## 南武历史
-[古南武史](./history/ancient.md)
-
-[近代南武史](./history/modern.md)
-
-[南武卫国战争](./history/war.md)
-
-[南武民主革命](./history/revolution.md)
-
-[南武联合党党史](./history/party.md)
-
-[南武特区建设史](./history/special_zone.md)
-
-[南武集团发展史](./history/nambu_group.md)
-
-[南武进步主义史](./history/progressivism.md)
-
-[南武中央人民政府建设史](./history/central_government.md)
-
-## 南武记忆
-### 国家相关
-[南武党、国、集团、特区相关的视觉设计](./memory/nambu_design.md)
-
-### 法规相关
-[南武民主共和国宪法](./memory/constitution.md)
-
-[南武联合党党章](./memory/party_constitution.md)
-
-[南武民主共和国集团领导法](./memory/group_leadership_law.md)
-
-[南武民主共和国特区法](./memory/special_zone_law.md)
-
-### 机关公文
-#### 南武联合党中央办公厅
-[关于进一步推动南武集团特色计划经济建设的意见(征求稿)](./memory/further_promoting_construction_of_planned_economy.md)
-
-#### 南武民主共和国中央发展规划委员会
-[关于在海実特区展开集团领导制实验性改革的方案](./memory/reform_of_group_leadership_system_in_kaijitsu.md)
-
----
 ### 免责声明
 
 * 本文库内容内容纯属娱乐与虚构，本站与现实中的国家、地区、组织、企业、团体、学校以及个人无任何关系，请勿带入现实生活
@@ -118,8 +33,11 @@
 * 本文库内容并无任何人身攻击及恶意倾向，部分内容使用人工智能大语言模型辅助创作
 * 本文库内容未经允许切勿转载，著作权归本文库的维护志愿者所有
 * 不得恶意利用本站资源从事非法活动及不正当的行为，造成的后果本站及创作者概不负责
+  :::
+
+::: tip
 
 ### 友情链接
-> 千百年来为自由奋斗的人永垂不朽！
 
-[乐课文档](https://docs.leke1234.com/)
+千百年来为自由奋斗的人永垂不朽：[乐课文档](https://docs.leke1234.com/)
+:::
